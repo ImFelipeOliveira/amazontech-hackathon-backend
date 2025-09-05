2213b6a63d12805d259cda68e35b985cd92c4061
Felipe Oliveira <flipeaz342@gmail.com>
2025-09-05 00:24:48 -0400

    chore: remove api prefix

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,6 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: true }));
-app.use("/api", authRoutes);
+app.use("/", authRoutes);
 
 export const api = functions.https.onRequest(app);
